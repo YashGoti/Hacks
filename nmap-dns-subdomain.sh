1c0cba9a4830da4e2eacd2830071857d9acba815
YashGoti <39102999+YashGoti@users.noreply.github.com>
2020-07-18 13:59:18 +0530

    Update nmap-dns-subdomain.sh

--- nmap-dns-subdomain.sh
+++ nmap-dns-subdomain.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#@_YashGoti_
 
 if [[ -z $1 ]]; then
 	echo "./nmap.sh DOMAIN"
 else
-	nmap --script dns-brute $1 -oX $1 > /dev/null
+	nmap --script dns-brute $1 --min-rate 100000 -oX $1 > /dev/null
 	cat $1 | grep $1 | cut -d '<' -f2 | cut -d '>' -f2 | grep . | sort -u
 	rm -rf $1
 fi
