11191f81de219dc3c94d2c473f128e6d47ce9189
YashGoti <39102999+YashGoti@users.noreply.github.com>
2020-07-18 14:00:28 +0530

    Find Subdomain Using Nmap Script | v1.0

--- nmap-dns-subdomain.sh
+++ nmap-dns-subdomain.sh
@@ -4,7 +4,7 @@
 if [[ -z $1 ]]; then
 	echo "./nmap.sh DOMAIN"
 else
-	nmap --script dns-brute $1 --min-rate 100000 -oX $1 > /dev/null
+	nmap --script dns-brute $1 --min-rate 50000 -oX $1 > /dev/null
 	cat $1 | grep $1 | cut -d '<' -f2 | cut -d '>' -f2 | grep . | sort -u
 	rm -rf $1
 fi
