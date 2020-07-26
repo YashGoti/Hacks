a4b39ac24e79591038daa96018ccea627b7bc8fa
YashGoti <39102999+YashGoti@users.noreply.github.com>
2020-07-26 11:05:08 +0530

    Update gal.sh

--- gal.sh
+++ gal.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
-singleurl(){
-	curl -siL $1 | sed -n -E "s/.*(href|src|url)[=:]['\"]?([^'\">]+).*/\2/p" | sort -fu > tmp.txt
-	while read -r uri;
-		do if [[ $(echo $uri | grep '^/') ]]; then echo $1$uri; elif [[ $(echo $uri | grep '^#') ]]; then :;else echo $uri; fi;
-	done < tmp.txt
-	rm -rf tmp.txt
-}
-listurl(){
-	while read -r url;
-		do
-			curl -siL $url | sed -n -E "s/.*(href|src|url)[=:]['\"]?([^'\">]+).*/\2/p" | sort -fu > tmp.txt
-			while read -r uri;
-				do if [[ $(echo $uri | grep '^/') ]]; then echo $url$uri; elif [[ $(echo $uri | grep '^#') ]]; then :;else echo $uri; fi;
-			done < tmp.txt
-			rm -rf tmp.txt;
-	done < $1
+banner(){
+	echo -e "\t                __\t"
+	echo -e "\t   ____  ____  / /\t"
+	echo -e "\t  / __ \/ __ \/ / \t"
+	echo -e "\t / /_/ / /_/ / /_ \t"
+	echo -e "\t \__, /\__,_/___/ \t"
+	echo -e "\t/____/            \t"
 }
 
-if [[ -z $1 ]] || [[ -z $2 ]]; then
-	echo "[Usage]: link.sh -u url"
-	echo "         link.sh -ul urllist.txt"
-	echo "[Warning]: URL formate should be http://target.com or https://target.com without slash in end of url"
-elif [[ $1 == "-u" ]]; then
-	singleurl $2
-elif [[ $1 == "-ul" ]]; then
-	listurl $2
-fi
+curl -siL $1 | grep -Eoi '<(a|link) [^>]+>' | grep -Eo 'href="[^\"]+"' | cut -d '=' -f2 | cut -d '"' -f2 | grep -v '#$' | grep -v '/$' | sort -u >> tmp.txt &
+curl -siL $1 | grep -Eoi '<script [^>]+>' | grep -Eoi 'src="[^\"]+"' | cut -d '=' -f2 | cut -d '"' -f2 | sort -u >> tmp.txt &
+curl -siL $1 | sed -n -E "s/.*(href|src|url|path)[=:]['\"]?([^'\">]+).*/\2/p" | grep -v '#$' | grep -v '/$' | sort -u >> tmp.txt &
+curl -siL $1 | grep -Po '(?<=href=")[^"]*(?=")' | grep -v '#$' | grep -v '/$' | sort -u >> tmp.txt &
+curl -siL $1 | grep -Eoi '(http|https)://[^ >]+' | cut -d '"' -f1 | sort -u >> tmp.txt &
+curl -siL $1 | grep -Po '(?<=src=")[^"]*(?=")' | grep -v '#$' | grep -v '/$' | sort -u &
+curl -siL $1 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u >> tmp.txt
+cat tmp.txt | grep $1 | sort -u
