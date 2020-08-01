7e039cf209459b0e3befd2213f7e2e723f0f49d5
YashGoti <39102999+YashGoti@users.noreply.github.com>
2020-08-02 01:20:31 +0530

    Update gal.sh

--- gal.sh
+++ gal.sh
@@ -26,7 +26,7 @@ getURLs(){
 	curl -siL $1 | grep -Eoi '<script [^>]+>' | grep -Eoi 'src="[^\"]+"' | cut -d '=' -f2 | cut -d '"' -f2 | sort -u >> tmp.txt &
 	curl -siL $1 | sed -n -E "s/.*(href|src|url|path)[=:]['\"]?([^'\">]+).*/\2/p" | grep -v '#$' | grep -v '/$' | sort -u >> tmp.txt &
 	curl -siL $1 | grep -Po '(?<=href=")[^"]*(?=")' | grep -v '#$' | grep -v '/$' | sort -u >> tmp.txt &
-	curl -siL $1 | grep -Eoi '(http|https)://[^ >]+' | cut -d '"' -f1 | sort -u >> tmp.txt &
+	curl -siL $1 | grep -Eoi '(http|https)://[^ >]+' | cut -d '"' -f1 | cut -d "'" -f1 | sort -u >> tmp.txt &
 	curl -siL $1 | grep -Po '(?<=src=")[^"]*(?=")' | grep -v '#$' | grep -v '/$' | sort -u &
 	curl -siL $1 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u >> tmp.txt
 	cat tmp.txt | grep $1 | sort -u
